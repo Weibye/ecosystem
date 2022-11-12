32092713c022ee1a64a63fb2ab4c8420500c1f8a
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2022-11-12 22:06:12 +0100

    Adding support for thirst + drinking of water (#6)

--- src/agent/needs.rs
+++ src/agent/needs.rs
@@ -11,6 +11,12 @@ pub(crate) struct Hunger {
     pub value: f32,
 }
 
+#[derive(Component, Debug, Copy, Clone)]
+pub(crate) struct Thirst {
+    pub per_second: f32,
+    pub value: f32,
+}
+
 /// System that decays all agents' hunger over time.
 pub(crate) fn hunger_decay(time: Res<Time>, mut q: Query<&mut Hunger>) {
     for mut hunger in &mut q {
@@ -18,6 +24,17 @@ pub(crate) fn hunger_decay(time: Res<Time>, mut q: Query<&mut Hunger>) {
 
         if hunger.value >= 100.0 {
             hunger.value = 100.0;
+        }
+    }
+}
+
+/// System that decays all agents' hunger over time.
+pub(crate) fn thirst_decay(time: Res<Time>, mut q: Query<&mut Thirst>) {
+    for mut thirst in &mut q {
+        thirst.value += thirst.per_second * time.delta_seconds();
+
+        if thirst.value >= 100.0 {
+            thirst.value = 100.0;
         }
     }
 }
