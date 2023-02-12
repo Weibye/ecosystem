3514a55f8e8c30f757ac4986bcab83894d51b2b3
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2023-02-12 13:36:28 +0100

    Migrate to big-brain 0.16

--- src/agent/actions.rs
+++ src/agent/actions.rs
@@ -7,8 +7,8 @@ use bevy::{
 };
 use bevy_turborand::{DelegatedRng, GlobalRng};
 use big_brain::{
-    prelude::ActionState,
-    thinker::{ActionSpan, Actor},
+    prelude::{ActionBuilder, ActionState},
+    thinker::{Action, ActionSpan, Actor},
 };
 use bracket_pathfinding::prelude::a_star_search;
 
@@ -24,31 +24,31 @@ use crate::{
 // ACTIONS
 
 /// Action that moves to a target.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct MoveAction;
 
 /// Action that figures out which food-source to get.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct FindFoodAction;
 
 /// Action that figures out which water-source to get.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct FindDrinkAction;
 
 /// Action that eats from a food source.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct EatAction;
 
 /// Action that drinks from a water source.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct DrinkAction;
 
 /// Action that reproduces and spawn an offspring.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct ReproduceAction;
 
 /// Action that simply wanders randomly.
-#[derive(Component, Debug, Clone)]
+#[derive(Component, Debug, Clone, ActionBuilder)]
 pub(crate) struct IdleAction;
 
 // Action targets
